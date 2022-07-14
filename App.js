691b5d579d352a945f3d5df2d0bcea8b94cff513
StarinovOleg <hromoi@inbox.ru>
2022-07-14 13:07:30 +0700

    axios, api, get

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import type {Node} from 'react';
+import {QueryClient, QueryClientProvider} from 'react-query';
 
 import ScreenOne from './src/ScreenOne';
 import ScreenSecond from './src/ScreenSecond';
@@ -7,17 +8,28 @@ import ScreenThree from './src/ScreenThree';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 const Stack = createNativeStackNavigator();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: true,
+      refetchOnMount: true,
+      cacheTime: 1000 * 60 * 60 * 24,
+    },
+  },
+});
 const App: () => Node = ({navigation}) => {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}>
-        <Stack.Screen name="ScreenOne" component={ScreenOne} />
-        <Stack.Screen name="ScreenSecond" component={ScreenSecond} />
-        <Stack.Screen name="ScreenThree" component={ScreenThree} />
-      </Stack.Navigator>
+      <QueryClientProvider client={queryClient}>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}>
+          <Stack.Screen name="ScreenOne" component={ScreenOne} />
+          <Stack.Screen name="ScreenSecond" component={ScreenSecond} />
+          <Stack.Screen name="ScreenThree" component={ScreenThree} />
+        </Stack.Navigator>
+      </QueryClientProvider>
     </NavigationContainer>
   );
 };
--- src/ScreenSecond/Shop.tsx
+++ src/ScreenSecond/Shop.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
 import ShopCart from './ShopCart';
-
-const shopItem = [
-  {price: 1, about: 'rose'},
-  {price: 2, about: 'rose'},
-  {price: 3, about: 'rose'},
-];
+import useGetProducts from '../hooks/get-product-hook';
 
 export default function Shop() {
-  const listItems = shopItem.map((item, index) => (
+  const products = useGetProducts();
+  const listItems = products?.map((item, index) => (
     <View style={styles.containerItem} key={index}>
-      <ShopCart price={item.price} about={item.about} />
+      <ShopCart
+        price={item.price}
+        about={item.product_name}
+        img={item.avatar}
+      />
     </View>
   ));
   return <View style={styles.container}>{listItems}</View>;
