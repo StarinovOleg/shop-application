0bd8097d3a7b692767d069f616365af610e3330c
StarinovOleg <hromoi@inbox.ru>
2022-07-15 10:37:07 +0700

    customer api

---
+++ api/customer-service.tsx
@@ -0,0 +1,25 @@
+import axios from 'axios';
+interface AddUser {
+  phone: string;
+  firstName: string;
+  order: {[key: string]: string};
+}
+export const CustomerService = {
+  async addcustomer({phone, firstName, order}: AddUser) {
+    const data = {
+      phone,
+      firstName,
+      order,
+    };
+    return axios
+      .post(`https://62cf6dde486b6ce8265685db.mockapi.io/users`, {
+        phone: data.phone,
+        firstName: data.firstName,
+        order: data.order,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then(res => res.data);
+  },
+};
